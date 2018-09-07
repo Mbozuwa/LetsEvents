80b30e76a9664ddaec6abfc5441cfd2e22695ac0
Alex Kalis <99039736@mydavinci.nl>
2018-09-07 17:18:21 +0200

    Fixed some bugs and changed some styles of the welcome blade.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,16 +48,16 @@
                             <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="http://via.placeholder.com/20?text=Placeholder.com+rocks!" class="img-circle" alt="Avatar"> <span></span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
                             @if (Auth::check())
                                 <ul class="dropdown-menu">
-                                    <li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
-                                    <li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
+                                    <li><a href="/profile/{{Auth::user()->id}}"><i class="lnr lnr-user"></i> <span>het profiel van {{Auth::user()->name}}</span></a></li>
                                     <li><a href="/logout"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
                                 </ul>
-                            @endif
+                            @else
+
                             <ul class="dropdown-menu">
                                 <li><a href="/user/signup"><i class="lnr lnr-user"></i> <span>Sign up</span></a></li>
                                 <li><a href="/user/signin"><i class="lnr lnr-cog"></i> <span>Sign in</span></a></li>
                             </ul>
-
+                            @endif
                         </li>
                     </ul>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,36 +2,21 @@
 @section('content')
             <div class="main-content">
                 <div class="container-fluid">
-                    <h3 class="page-title">Welkom</h3>
+                    <h3 class="page-title" style="color: black;">Welkom, bij de hoofd pagina.</h3>
                     <div class="row">
                         <div class="col-md-8">
                             <!-- BASIC TABLE -->
                             <div class="panel">
                                 <div class="panel-body">
-                                </div>
-                            </div>
+
                             <div id="calendar"></div>
                                 <script type="text/javascript" src="{{ URL::asset('js/moment.min.js') }}"></script>
                                 <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>
                                 <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
-
                         </div>
+                    </div>
+                </div>
                             <!-- END BASIC TABLE -->
-                        </div>
-                        <div class="col-md-3">
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">-</h3>
-                                </div>
-                                <div class="panel-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                </div>
-                            </div>
                         </div>
                     </div>
                 </div>
