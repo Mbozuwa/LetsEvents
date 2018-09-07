fcb79c21b0fc4c561bb254607eddca4e549a5bee
Raymond Wijkhuizen <99036929@mydavinci.nl>
2018-09-07 11:59:23 +0200

    commented out non working code for showcase

--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -10,7 +10,7 @@
                         <p>Er zijn {{$count}} van de {{$event['max_participant']}} studenten die gaan</p>
                     </div>
                     <div class="col-md-4" style="background-color:white;">
-                        @if (!empty($user))
+                        <!-- @if (!empty($user)) -->
                         @if (empty($attendence[0]))
                         <h2>Laat weten of je komt</h2><br>
                         @else 
@@ -24,10 +24,10 @@
                         <h3>Je huidige status is <b>{{$attendence[0]['status']}}</b></h3>
                         <h4>Status aanpassen, klik hier</h4>
                         @endif
-                        @else 
+                        <!-- @else 
                         <h3>U bent op het moment niet ingelogd</h3>
                         <a href="/user/signin"><h4>klik hier om naar het login scherm te gaan</h4></a>
-                        @endif
+                        @endif -->
                     </div>
                     <div class="col-md-2" >
                         
