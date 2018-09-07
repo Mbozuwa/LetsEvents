50efb86681f27eb8616ae3687bd76610207e131a
Laurens <99005216@mydavinci.nl>
2018-09-07 11:36:45 +0200

    Calendar page update / Style update to not mess with normal style / Using latest fontAwesome.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0">
 
     <!-- BOOTSTRAP, FONT-AWESOME, LETS EVENT CSS -->
-    <link rel="stylesheet" href="/assets/vendor/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="/assets/vendor/font-awesome/css/font-awesome.min.css">
-    <link rel="stylesheet" href="/assets/vendor/linearicons/style.css">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('assets/vendor/linearicons/style.css') }}">
     <link rel="stylesheet" href="{{ asset('/css/style_alex.css') }}">
 
     <!-- CALENDAR -->
@@ -17,7 +17,8 @@
 
     <!-- FONTS -->
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700" rel="stylesheet">
-    <!-- Scripts -->
+
+    <!-- SCRIPTS -->
     <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
 </head>
 <body>
@@ -45,19 +46,19 @@
                             </ul>
                         </li>
                         <li class="dropdown">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="http://via.placeholder.com/20?text=Placeholder.com+rocks!" class="img-circle" alt="Avatar"> <span></span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><img src="http://via.placeholder.com/20?text=Placeholder.com+rocks!" class="img-circle" alt="Avatar"> <span>{{ Auth::user()->name }}</span> <i class="icon-submenu lnr lnr-chevron-down"></i></a>
                             @if (Auth::check())
                                 <ul class="dropdown-menu">
                                     <li><a href="#"><i class="lnr lnr-user"></i> <span>My Profile</span></a></li>
                                     <li><a href="#"><i class="lnr lnr-cog"></i> <span>Settings</span></a></li>
                                     <li><a href="/logout"><i class="lnr lnr-exit"></i> <span>Logout</span></a></li>
                                 </ul>
-                            @endif
+                            @else
                             <ul class="dropdown-menu">
                                 <li><a href="/user/signup"><i class="lnr lnr-user"></i> <span>Sign up</span></a></li>
                                 <li><a href="/user/signin"><i class="lnr lnr-cog"></i> <span>Sign in</span></a></li>
                             </ul>
-
+                            @endif
                         </li>
                     </ul>
                 </div>
@@ -84,9 +85,9 @@
             </div>
         </footer>
     </div>
-    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js"></script>
-    <script src="assets/scripts/klorofil-common.js"></script>
+    <script src="{{ asset('assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('assets/vendor/jquery-slimscroll/jquery.slimscroll.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/klorofil-common.js') }}"></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.17.1/moment.min.js'></script>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.1.0/fullcalendar.min.js'></script>
 </body>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,48 +1,30 @@
 @extends('layouts.app')
 @section('content')
-            <div class="main-content">
-                <div class="container-fluid">
-                    <h3 class="page-title">--</h3>
-                    <div class="row">
-                        <div class="col-md-8">
-                            <!-- BASIC TABLE -->
-                            <div class="panel">
-                                <div class="panel-body">
-                                </div>
-                            </div>
-                            <div id="calendar"></div>
-                                <script type="text/javascript" src="{{ URL::asset('js/moment.min.js') }}"></script>
-                                <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>
-                                <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
+<div class="main-content">
+    <div class="container-fluid">
+        <!-- OVERVIEW -->
+        <div class="panel panel-headline">
+            <div class="panel-heading">
+                <h3 class="panel-title">Dashboard</h3>
+                <p class="panel-subtitle">You are logged in!</p>
+            </div>
+        </div>
+        <!-- END OVERVIEW -->
 
-                        </div>
-                            <!-- END BASIC TABLE -->
-                        </div>
-                        <div class="col-md-3">
-                            <div class="panel">
-                                <div class="panel-heading">
-                                    <h3 class="panel-title">-</h3>
-                                </div>
-                                <div class="panel-body">
-                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                </div>
-                            </div>
-                        </div>
+        <div class="row">
+            <div class="col-md-6">
+                <!-- EVENT CALENDAR -->
+                <div class="panel">
+                    <div class="panel-body">
+                        <div id="calendar"></div>
+                        <script type="text/javascript" src="{{ URL::asset('js/moment.min.js') }}"></script>
+                        <script type="text/javascript" src="{{ URL::asset('js/index.js') }}"></script>
+                        <link rel="stylesheet" type="text/css" href="{{ URL::asset('css/style.css') }}">
                     </div>
                 </div>
+                <!-- END EVENT CALENDAR -->
             </div>
-            <body>
-                <div class="flex-center position-ref full-height">
-                    <div class="content" style="margin-right: 400px">
-                        <div class="title m-b-md">
-                            Let's Event
-                        </div>
-
-                </div>
-            </body>
+        </div>
+    </div>
+</div>
 @endsection
