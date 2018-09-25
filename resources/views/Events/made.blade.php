3ad74a7049e89b92bd2c96d89f68cc7f920491c0
Alex Kalis <99039736@mydavinci.nl>
2018-09-25 15:28:50 +0200

    Style changed

--- resources/views/Events/made.blade.php
+++ resources/views/Events/made.blade.php
@@ -20,13 +20,13 @@
                     {{-- {{dd($event)}} --}}
                     <div class="flex-center position-ref full-height" >
                         <div class="content" style="background-color: white; padding:10px; margin-right:10px;">
-                        <div class="col-md-6" style="background-color: white;">
+                        <div  style="background-color: white;">
                           <h1>Het evenement: {{$event->name}}</h1>
                           <h2 class="card-text mb-auto">beschrijving: {{$event->description}}</h2>
-                          <h2>Hoeveel mensen mogen mee doen: {{$event->max_participant}}</h2>
-                          <h3>Begint: {{ \Carbon\Carbon::parse($event->begin_time)->format('d-m-Y H:i')}}</h3>
-                          <h3>Eindigt: {{ \Carbon\Carbon::parse($event->end_time)->format('d-m-Y H:i')}}</h3>
-                          <h3>Het adres:{{$event->address}},{{$event->place}}</h3>
+                          <h2>Maximum aantal deelnemers: {{$event->max_participant}}</h2>
+                          <h3>Begin tijd: {{ \Carbon\Carbon::parse($event->begin_time)->format('d-m-Y H:i')}}</h3>
+                          <h3>Eind tijd: {{ \Carbon\Carbon::parse($event->end_time)->format('d-m-Y H:i')}}</h3>
+                          <h3>Adres:{{$event->address}},{{$event->place}}</h3>
                           {{-- <h3>De categorie:{{$categories->event}}</h3> --}}
                           @if ($event->payment > 0)
                               <h3>De prijs:{{$event->payment}}</h3>
@@ -38,9 +38,6 @@
                           @if (Auth::check())
                               <p><a href="/delete/{{$event->id}}">Verwijder</a></p>
                           @endif
-                      </div>
-                      <div class="col-md-6" >
-                          <img src="/assets/img/login-bg.jpg" style="height: 47px; width: 600px; margin-bottom: 20px">
                       </div>
                     </div>
                     <br>
