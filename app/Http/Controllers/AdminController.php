a140be1a88f8b4bdd2c049539b6d12825a821d4e
Raymond Wijkhuizen <99036929@mydavinci.nl>
2018-10-03 09:52:31 +0200

    Admin bug fix

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -9,20 +9,31 @@ class AdminController extends Controller
 {
     public function index()
     {
-        if (Auth::user()->role_id != 2) {
-        	return redirect('/');
+        if (Auth::check()) {
+            if (Auth::user()->role_id != 2) {
+            return redirect('/');
         }
         $users = \App\user::all();
         $activities = "";
         return view('admin' ,['users' => $users, 'activities' => $activities]);
+        }
+        else {
+            return redirect('/logout');
+        }
+        
     }
     public function activity($id)
     {
-        if (Auth::user()->role_id != 2) {
-        	return redirect('/');
+        if (Auth::check()) {
+            if (Auth::user()->role_id != 2) {
+            return redirect('/');
         }
         $users = \App\user::all();
-        $activities =\App\Event::where('user_id', $id)->get();
+        $activities = "";
         return view('admin' ,['users' => $users, 'activities' => $activities]);
+        }
+        else {
+            return redirect('/logout');
+        }
     }
 }
