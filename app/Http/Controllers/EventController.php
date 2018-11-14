1cad7f71d50671a3f1ece408f78fc3ad8bee902f
JASPER-PC\Gebruiker <99027698@mydavinci.nl>
2018-11-14 14:07:51 +0100

    css inproved

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -10,6 +10,7 @@ use App\CategoryEvent;
 use App\Category;
 use App\User;
 use Auth;
+use Validator;
 
 use \File;
 
@@ -105,10 +106,9 @@ class EventController extends Controller
 
     public function store(Request $request) {
         session(['rememberEvent' => $request->all()]);
-        // var_dump(session('rememberEvent'));
-        // die;
+       
         $user = Auth::user();
-        $request->validate([
+        $validator = Validator::make($request->all(),[
             'name' => 'required|max:50',
             'description' => 'required|max:1400',
             'place' => 'required|alpha',
@@ -116,8 +116,15 @@ class EventController extends Controller
             'max_participant' => 'required|alpha_num',
             'begin_time' => 'required',
             'end_time' => 'required',
-            'image' => 'required'
+            'image' => 'required',
         ]);
+        if ($validator->fails())
+        {
+            return redirect()->back()->with($validator)->withInput();
+        }
+        //if else in of buiten de request
+        
+
         $post = new Event();
         $post->name = $request->input('name');
         $post->description = $request->input('description');
@@ -163,7 +170,7 @@ class EventController extends Controller
 
         $post->user_id = $user->id;
         $post->save();
-        return redirect('/events/index' . $post->name);
+        return redirect('/events/index');
     }
 
     /**
