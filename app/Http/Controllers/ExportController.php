fed1e8bb0ada5960f641a2008871ccbcc77b7aa5
Laurens <99005216@mydavinci.nl>
2018-12-11 13:29:25 +0100

    "Working" excel download.

--- app/Http/Controllers/ExportController.php
+++ app/Http/Controllers/ExportController.php
@@ -8,15 +8,15 @@ use Maatwebsite\Excel\Exporter;
 
 class ExportController extends Controller
 {
-	public function export($id)
+	/*public function export($id)
 	{
 	    $exporter = app()->makeWith(UsersExport::class, compact('id'));   
 	    return $exporter->download('participants_event.xlsx');
-	}
+	}*/
     
-    /*function export($id)
+    function export()
     {
     	//$registered = Registration::where(['event_id' => $id])->where('status' , "Ik ga")->get();
         return Excel::download(new UsersExport(), 'participants_event.xlsx');
-    }*/
+    }
 }
