edeaceaea86223b350b78d82b1939dfaddf680c6
Laurens <99005216@mydavinci.nl>
2018-12-14 18:18:49 +0100

    Refactoring SchoolController, code comments and PSR standard.

--- resources/views/schools/index.blade.php
+++ resources/views/schools/index.blade.php
@@ -2,24 +2,26 @@
 @section('content')
         <div class="main-content">
             <div class="container-fluid">
-            @if(session()->has('message'))
+            @if(session('success'))
                 <div class="alert alert-success">
-                    {{ session()->get('message') }}
+                    {{ session('success') }}
                 </div>
-            @elseif(session()->has('error'))
+            @elseif(session('error'))
                 <div class="alert alert-danger">
-                    {{ session()->get('error') }}
+                    {{ session('error') }}
                 </div>
             @endif
             </div>
+
             <div class="container-fluid">
                 <h3 class="page-title">{{ __('msg.school.schools') }}</h3>
+                {{ $schools->links() }}
                 <div class="row">
                 @foreach ($schools as $school)
                     <div class="col-md-6">
                         <div class="panel">
                             <div class="panel-heading">
-                                <h2 class="panel-title"><a href="/school/edit/{{ $school->id }}">{{ $school->name }}</a></h2>
+                                <h2 class="panel-title"><a href="{{ action('SchoolController@edit', ['id' => $school->id]) }}">{{ $school->name }}</a></h2>
                             </div>
                             <div class="panel-body">
                                 <div class="main-info-item">
@@ -30,19 +32,24 @@
                                     <span class="title">{{ __('msg.address') }}:</span>
                                     <span class="value">{{ $school->address }}</span>
                                 </div>
-                                <a href="/school/edit/{{ $school->id }}">{{ __('msg.school.edit') }}</a>
-                                <a onclick="popup('{{ $school->id }}')" href="#">{{ __('msg.school.delete') }}</a>
+
+                                <div style="float:right;">
+                                    <a class="btn btn-default btn-xs" href="{{ action('SchoolController@edit', ['id' => $school->id]) }}" role="button">
+                                        <i class="fas fa-pencil-alt"></i> {{ __('msg.school.edit') }}
+                                    </a>
+
+                                    <form action="{{action('SchoolController@destroy', ['id' => $school->id])}}" method="post" style="display:inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button class="btn btn-danger btn-xs" onclick="return confirm('{{ __('msg.school.confirm') }}');" type="submit">
+                                            <i class="far fa-trash-alt"></i> {{ __('msg.school.delete') }}
+                                        </button>
+                                    </form>
+                                </div>
                             </div>
                         </div>
                     </div>
                 @endforeach
-                    <script>
-                        function popup(id) {
-                            if (confirm("{{ __('msg.school.confirm') }}")) {
-                                window.location.href = "/school/delete/" + id;
-                            }
-                        }
-                    </script>
                 </div>
             </div>
         </div>
